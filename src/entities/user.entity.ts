36d6d8cf1e472f028e3e7e65049713b575550a3b
emilekamana <e.kamana@alustudent.com>
2023-02-10 14:32:11 +0200

    create user entity and schema

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,39 @@
+import { Entity, Index, Column } from "typeorm";
+import Model from "./model.entity";
+
+export enum RoleEnumType {
+    USER = 'user',
+    ADMIN = 'admin',
+  }
+
+@Entity('users')
+export class User extends Model {
+    @Column()
+    userName: string;
+  
+    @Index('email_index')
+    @Column({
+      unique: true,
+    })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column({
+        type: 'enum',
+        enum: RoleEnumType,
+        default: RoleEnumType.USER,
+    })
+    role: RoleEnumType.USER;
+
+    @Column({
+    default: false,
+    })
+    verified: boolean;
+
+    toJSON() {
+        return { ...this, password: undefined, verified: undefined };
+    }
+
+}
